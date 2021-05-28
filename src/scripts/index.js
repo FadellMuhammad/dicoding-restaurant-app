2ea6bd164ef8c4f00bca7f822e02d41b02d9c8eb
FadellMuhammad <fadel54031@gmail.com>
2021-05-28 11:22:08 +0700

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -16,10 +16,12 @@ data.restaurants.forEach((restoran) => {
     html += `
         <div class="card">
             <h6 tabindex="0">${restoran.city}</h6>
-            <img src="${restoran.pictureId}" alt="foto khas berbagai daerah" tabindex="0">
-            <h5 tabindex="0">Rating : ${restoran.rating}</h5>
-            <h3 tabindex="0">${restoran.name}</h3>
-            <p tabindex="0">${restoran.description}</p>
+            <img src="${restoran.pictureId}" alt="${restoran.name}" tabindex="0">
+  
+                <h5 tabindex="0">Rating : ${restoran.rating}</h5>
+                <h3 tabindex="0">${restoran.name}</h3>
+                <p tabindex="0">${restoran.description}</p>
+     
         </div>
     `;
 })
